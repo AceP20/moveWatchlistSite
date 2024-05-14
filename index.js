e01069f889f73cb4212cf5cf92f4c2997922615d
AceP20 <alvaro.polanco20@gmail.com>
2024-05-14 16:51:28 -0400

    finished basic function of index page as well as refactored the code to display more movies and styled for mobile only

--- index.js
+++ index.js
@@ -1,19 +1,57 @@
 // http://www.omdbapi.com/?i=tt3896198&apikey=6bc2ab8f api key
-const indexSearchInput = document.getElementById("index-search-input").value
 
-document.getElementById("search-button").addEventListener("click",async () => {
-    const res = await fetch(`http://www.omdbapi.com/?i=tt3896198&apikey=6bc2ab8f&t=${indexSearchInput}`)
-    const data = await res.json()
+// variables
+const indexSearchInput = document.getElementById("index-search-input")
+const searchButton = document.getElementById("search-button")
+const indexMovieDisplay = document.getElementById("index-movie-display")
 
-    document.getElementById("index-movie-display").innerHTML = `
-    <img id="movie-poster" src="${data.Poster}" >
-    <div id="movie-details">
-        <h3 id="movie-title">${data.Title} </h3> 
-        <p id="movie-review">${data.Ratings[0].Value}</p>
-        <p id="movie-length">${data.Runtime}</p>
-        <p id="movie-genre">${data.Genre}</p>
-        <button id="add-to-watchlist-button">+ Watchlist</button>
-        <p id="movie-plot">${data.Plot}</p>
-    </div>
-        `
-})
+// Functions
+const getMovies = async () => {
+    if (indexSearchInput != "") {
+        try {
+            const res = await fetch(`http://www.omdbapi.com/?apikey=6bc2ab8f&s=${indexSearchInput.value}`)
+            const data = await res.json()
+            
+            //list movie result imdb IDs for later reference
+            const moviesIDArray = []
+            data.Search.forEach(movie => moviesIDArray.push(movie.imdbID))
+
+            //grab the movie detail objects and put into its own array
+            const movieDetailsArr = []
+            for(currentMovie in moviesIDArray){
+                const response = await fetch(`http://www.omdbapi.com/?apikey=6bc2ab8f&i=${moviesIDArray[currentMovie]}`)
+                movieDetailsArr.push(await response.json())
+            }
+            
+            renderMovies(movieDetailsArr)
+
+        } catch (error) {
+            console.error("Error fetching data:", error)
+        }
+    }
+}
+
+renderMovies = (movieObjectArr) => {
+    let moviesHtml = ""
+    for (let i = 0; i < movieObjectArr.length; i++) {
+        moviesHtml += `
+        <div class="individual-movie-section">
+            <img class="movie-poster" src="${movieObjectArr[i].Poster}" >
+            <div class="movie-details">
+                <h3 class="movie-title">${movieObjectArr[i].Title} </h3> 
+                <p class="movie-review"><i class="fa-regular fa-star fa-small"></i>${movieObjectArr[i].Ratings[0].Value}</p>
+                <p class="movie-length">${movieObjectArr[i].Runtime}</p>
+                <p class="movie-genre">${movieObjectArr[i].Genre}</p>
+                <button class="buttons">+ Watchlist</button>
+                <p class="movie-plot">${movieObjectArr[i].Plot}</p>
+            </div>
+        </div>
+            `
+    }
+    indexMovieDisplay.innerHTML = moviesHtml
+}
+
+// Event listeners
+searchButton.addEventListener("click", getMovies)
+
+
