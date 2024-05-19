77db0f1b5a9a0a20b55869bef7ee4fadc41523ac
AceP20 <alvaro.polanco20@gmail.com>
2024-05-18 23:07:08 -0400

    Updated the rendering functions to dynamically work and change the visual of the add/remove button or html display itself depending on page

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 // http://www.omdbapi.com/?i=tt3896198&apikey=6bc2ab8f api key
 
-// variables
+/*****
+UNIVERSAL VARIABLES DECLATIONS 
+*****/
+
 const indexSearchInput = document.getElementById("index-search-input")
 const searchButton = document.getElementById("search-button")
 const indexMovieDisplay = document.getElementById("index-movie-display")
+const watchlistMovieDisplay = document.getElementById("watchlist-movie-display")
 
-// Functions
+/***** 
+FUNCTIONS 
+*****/
 
 //uses user input to get movies from API
 const getMovies = async () => {
@@ -25,7 +31,7 @@ const getMovies = async () => {
                 movieDetailsArr.push(await response.json())
             }
 
-            renderMovies(movieDetailsArr)
+            renderMovies(movieDetailsArr, indexMovieDisplay)
 
         } catch (error) {
             console.error("Error fetching data:", error)
@@ -34,54 +40,109 @@ const getMovies = async () => {
 }
 
 //renders any movies from the response from the API search
-renderMovies = (movieObjectArr) => {
+renderMovies = (movieObjectArr, page) => {
     let moviesHtml = ""
 
     //creates a card of info about each movie from the get response
     for (let i = 0; i < movieObjectArr.length; i++) {
         //replaces any apostrophes for the html to update corrently 
-        const jsonString = JSON.stringify(movieObjectArr[i]).replaceAll("'", "&apos;")
+        const jsonStr = JSON.stringify(movieObjectArr[i]).replaceAll("'", "&apos;")
 
-        moviesHtml += `
+        moviesHtml += (`
         <div class="individual-movie-section">
         <img class="movie-poster" src="${movieObjectArr[i].Poster}" >
-        <div class="movie-details">
+        <div id=${movieObjectArr[i].imdbID} class="movie-details">
         <h3 class="movie-title">${movieObjectArr[i].Title} </h3> 
         <p class="movie-review"><i class="fa-regular fa-star fa-small"></i>${movieObjectArr[i].Ratings[0].Value}</p>
         <p class="movie-length">${movieObjectArr[i].Runtime}</p>
         <p class="movie-genre">${movieObjectArr[i].Genre}</p>
-        <button class="buttons add-to-watchlist" data-movie-details='${jsonString}' data-movie-title="${movieObjectArr[i].Title}">+ Watchlist</button>
+        <button class="buttons add-to-watchlist" 
+            data-movie-details='${jsonStr}' 
+            data-movie-title="${movieObjectArr[i].Title}"
+            data-on-watchlist=${checkIfAdded(movieObjectArr[i].imdbID)}>
+        </button>
         <p class="movie-plot">${movieObjectArr[i].Plot}</p>
         </div>
         </div>
-        `
+        `)
     }
+    page.innerHTML = moviesHtml
+    updateWatchlistButton()
+}
 
-    indexMovieDisplay.innerHTML = moviesHtml
+function updateWatchlistButton() {
+    const buttons = document.querySelectorAll(".add-to-watchlist")
+    buttons.forEach(button => {
+        const movieId = button.closest(".movie-details").id
+        if (checkIfAdded(movieId)) {
+            button.dataset.onWatchlist = true
+            button.textContent = "- Watchlist"
+        } else {
+            button.dataset.onWatchlist = false
+            button.textContent = "+ Watchlist"
+        }
+    })
+}
+
+function checkIfAdded(movieId) {
+    for (let i = 0; i < localStorage.length; i++) {
+        if (movieId === JSON.parse(localStorage.getItem(localStorage.key(i))).imdbID) {
+            return true
+        }
+    }
+    return false
 }
 
 //adds to local storage for future reference on watchlist page
-addToWatchlist = (movieDetailsStr) => {
+updateWatchlist = (movieDetailsStr, onWatchlist) => {
     const movieDetailsObj = JSON.parse(movieDetailsStr)
-    localStorage.setItem(movieDetailsObj.Title, movieDetailsStr)
+    if(onWatchlist === "false"){
+        localStorage.setItem(movieDetailsObj.imdbID, movieDetailsStr)
+        updateWatchlistButton()
+        renderPersonalWatchlist()
+    } else if(onWatchlist === "true") {
+        localStorage.removeItem(movieDetailsObj.imdbID)
+        updateWatchlistButton()
+        renderPersonalWatchlist()
+    }
 }
 
-// Event listener
+const renderPersonalWatchlist = () => {
+    const moviesInLocalStorage = []
+    for (let i = 0; i < localStorage.length; i++) {
+        moviesInLocalStorage.push(JSON.parse(localStorage.getItem(localStorage.key(i))))
+    }
+    renderMovies(moviesInLocalStorage, watchlistMovieDisplay)
+}
+
+/***** 
+EVENT LISTENER 
+*****/
+
 document.addEventListener("click", (e) => {
     //does both get movies from API function and then instantly renders(nested inside) onto the page
-    if (e.target.id == "search-button") {
+    if (e.target.id === "search-button") {
         getMovies()
     }
 
     //runs function that saves a stringified movie object to local storage
     if (e.target.classList.contains("add-to-watchlist")) {
-        addToWatchlist(e.target.dataset.movieDetails)
-    }
+            if(e.target.dataset.onWatchlist === "false"){
+                updateWatchlist(e.target.dataset.movieDetails, e.target.dataset.onWatchlist)
+            } else if(e.target.dataset.onWatchlist === "true"){
+                updateWatchlist(e.target.dataset.movieDetails, e.target.dataset.onWatchlist)
+            }
 
+    }
 })
 
+document.addEventListener("DOMContentLoaded", () => {
+    if (window.location.pathname === "/watchlist.html"){
+        renderPersonalWatchlist()
+    }
+})
 /* ***TO-DO***
-disable duplicates from being added to local storage by comparing value(movieDetails)
-move over to watchlist html to render any movies that have been stored in local storage
-
+Done -> move over to watchlist html to render any movies that have been stored in local storage 
+Done -> update the plus/minus sign on watchlist button depending on wether movies on localstorage or not (most likely another function)
+style personal watchlist html and then style both HTMLs for bigger sizes
 */
